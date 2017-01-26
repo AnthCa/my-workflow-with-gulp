20a38160bca80cfd314fbaa96be84248f886b425
Anthony Canales <anthonycca8@gmail.com>
2017-01-26 12:46:38 -0300

    comments on gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
+// funcion o tarea para utilizar browser-sync
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
         server: "./app"
@@ -11,7 +12,7 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
-// Función o tarea para compilar los archivos .scss en .css en el directorio especifico
+// Función o tarea para compilar los archivos .scss en .css en el directorio especifico (live)
 gulp.task('sass', function(){
   return gulp.src('scss/**/*.scss')
     .pipe(sass())
