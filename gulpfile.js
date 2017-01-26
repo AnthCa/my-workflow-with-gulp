2aa66f48ef537af090ced009d8a26fd57debf94c
Anthony Canales <anthonycca8@gmail.com>
2017-01-26 12:43:31 -0300

    first commit for my workflow with gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Función o tarea para compilar los archivos .scss en .css en el directorio especifico
+gulp.task('sass', function(){
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
+});
+
+//Funcion watch para automatizar la compilación de css
+gulp.task('watch', function(){
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
