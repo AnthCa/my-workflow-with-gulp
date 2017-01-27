9352520e2ef2fe72e30106473d30c3c48b1a779a
Anthony Canales <anthonycca8@gmail.com>
2017-01-27 02:58:53 -0300

    updating my workflow with css-min, image-min

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var cssnano  = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var autoprefixer = require('gulp-autoprefixer');
 
 // funcion o tarea para utilizar browser-sync
 gulp.task('serve', ['sass'], function() {
@@ -12,15 +15,37 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
-// Función o tarea para compilar los archivos .scss en .css en el directorio especifico (live)
+// Tarea o funcion para automatizar prefijos css
+gulp.task('autoprefix', () =>
+    gulp.src('scss/**/*.scss')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css/'))
+);
+
+//Función para minimizar imágenes
+gulp.task('optimize', () =>
+    gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('app/img'))
+);
+
+// Funcion para automatizar compilado de css, minificado de css, browsersync(live reload)
 gulp.task('sass', function(){
   return gulp.src('scss/**/*.scss')
     .pipe(sass())
+    // .pipe(cssnano())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: true
+    }))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.stream());
 });
 
-//Funcion watch para automatizar la compilación de css
-gulp.task('watch', function(){
-  gulp.watch('scss/**/*.scss', ['sass']);
-});
+// //Funcion watch para automatizar la compilación de css
+// gulp.task('watch', function(){
+//   gulp.watch('scss/**/*.scss', ['sass']);
+// });
